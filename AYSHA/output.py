0be0fc993ab6cc2e5d21dd4e7672dd41bc6db0f2
ayshasaniya <88335136+ayshasaniya@users.noreply.github.com>
2021-08-08 21:06:16 +0530

    Add files via upload

---
+++ AYSHA/output.py
@@ -0,0 +1,6 @@
+Python 3.6.0 (v3.6.0:41df79263a11, Dec 23 2016, 08:06:12) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+===== RESTART: C:\Users\HEWLETT PACKARD\Desktop\AYSHA\output\output.txt =====
+Aysha,saniyarcot123@gmail.com,@aysha,transcriptomics,@aysha_saniya,7
+>>> 
